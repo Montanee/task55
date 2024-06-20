e8d72f5fe7353331c1d603d3dcc08ed0a9700a02
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:44:35 +0300

    Добавьте файлы проекта.

---
+++ task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperationSelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер операции: 1. Сложение 2. Вычитание 3. Умножение");
+            int operationNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (operationNumber)
+            {
+                case 1:
+                    Console.WriteLine("Вы выбрали операцию: Сложение");
+                    break;
+                case 2:
+                    Console.WriteLine("Вы выбрали операцию: Вычитание");
+                    break;
+                case 3:
+                    Console.WriteLine("Вы выбрали операцию: Умножение");
+                    break;
+                default:
+                    Console.WriteLine("Операция неопределена");
+                    break;
+            }
+        }
+    }
+}
